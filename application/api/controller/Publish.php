ff7603906a8a2f57f6087c69c8abe2e92cd6a287
zhuozzz000 <1025713522@qq.com>
2018-11-11 17:04:55 +0800

    publish 1.0

---
+++ application/api/controller/Publish.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouzzz
+ * Date: 2018/11/11
+ * Time: 13:11
+ */
+
+namespace app\api\controller;
+
+
+use app\api\exception\ImageUploadException;
+use app\api\model\Images;
+use app\api\service\Image;
+use think\Controller;
+use think\Request;
+use \app\api\model\Publish as PublishModel;
+class Publish extends Controller
+{
+    public function add(Request $request)
+    {
+         $id = $request->id;
+         $image = $request->param('image');
+         $content = $request->param('content');
+         $bool = false;
+         if ($image != null){
+             $imgID = Image::handleImages($image);
+             $bool = (new PublishModel())->save([
+                 'uid' => $id,
+                 'content' => $content,
+                 'images' => json_encode($imgID),
+             ]);
+         }else{
+             $bool = (new PublishModel())->save([
+                 'uid' => $id,
+                 'content' => $content,
+             ]);
+         }
+         return json([
+             'msg' => $bool,
+         ]);
+    }
+}
