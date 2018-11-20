eb898a0e3ac2db76e7618aaa4be378250dec76d2
zhuozzz000 <1025713522@qq.com>
2018-11-20 11:39:31 +0800

    11-20 zhou 2

--- application/api/validate/UserSignInValidate.php
+++ application/api/validate/UserSignInValidate.php
@@ -14,8 +14,8 @@ class UserSignInValidate extends BaseValidate
     protected $rule = [
         'nick' => ['require','min'=> 3,'max' => 25,'regex' => '/^[\w]+/'],
         'password' => ['require','min'=>'6','max'=>'16','regex' => '/^[\d+|\w]*/'],
-        'sex' => ['require','in'=>'1,2'],
-        'age' => ['require','integer','between'=>'10,70'],
+        'sex' => ['require','in'=>'0,1,2'],
+        'age' => ['require','integer','between'=>'0,70'],
         'major' => ['chsAlpha'],
         'grade' => ['integer','between'=>'1,99'],
         'email' => ['require','email'],
